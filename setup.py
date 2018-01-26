3daaf15fd11c9cdc35b6cef17a4a3e861a6f9d50
laloutrejoyeuse <laloutrejoyeuse@imacfhe.local>
2018-01-26 11:01:41 +0100

    release

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 from setuptools import setup, find_packages
 
+from codecs import open  # To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setup(
     name='digimat.espa',
-    version='0.1.8',
-    description='Digimat Espa 4.4.4',
+    version='0.1.9',
+    description='Digimat ESPA 4.4.4',
+    long_description=long_description,
     namespace_packages=['digimat'],
     author='Frederic Hess',
-    author_email='fhess@splust.ch',
-    url='http://www.digimat.ch',
+    author_email='fhess@st-sa.ch',
+    url='https://github.com/digimat/digimat-espa',
     license='PSF',
     packages=find_packages('src'),
     package_dir={'': 'src'},
